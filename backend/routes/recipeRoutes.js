b0b2e747e0f911aaf8986e418ee840a1f4555f30
Prateeksha <prateekshakumari937@gmail.com>
2026-07-14 13:28:25 +0530

    Connected React frontend with Express backend

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,17 +1,30 @@
 const express = require("express");
+
 const router = express.Router();
 
+const recipes = [
+  {
+    id: 1,
+    title: "Classic Margherita Pizza",
+    category: "Pizza",
+    time: "30 mins",
+    rating: 4.8,
+    difficulty: "Easy",
+    image: "https://images.unsplash.com/photo-1513104890138-7c749659a591"
+  },
+  {
+    id: 2,
+    title: "Creamy Alfredo Pasta",
+    category: "Pasta",
+    time: "25 mins",
+    rating: 4.7,
+    difficulty: "Easy",
+    image: "https://images.unsplash.com/photo-1645112411341-6c4fd023714a"
+  }
+];
+
 router.get("/", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      title: "Pizza",
-    },
-    {
-      id: 2,
-      title: "Burger",
-    },
-  ]);
+  res.json(recipes);
 });
 
 module.exports = router;
