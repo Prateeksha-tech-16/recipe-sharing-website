dbe7701c6f3210a3798c86e98baf655f3c7a0ab2
Prateeksha <prateekshakumari937@gmail.com>
2026-07-15 22:31:08 +0530

    Completed frontend improvement and started backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
+require("dotenv").config();
+
 const express = require("express");
-const cors =require("cors");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const recipeRoutes = require("./routes/recipeRoutes");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-const recipeRoutes = require("./routes/recipeRoutes");
 app.use("/api/recipes", recipeRoutes);
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("RecipeShare Backend Running 🚀");
 });
 
-// Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
